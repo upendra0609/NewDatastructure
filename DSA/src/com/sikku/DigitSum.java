1131659ccc6b29011da7d608b4e4289b990c7122
Upendra <upendrasatna@gmail.com>
2023-09-02 23:22:20 +0530

    addSum

---
+++ DSA/src/com/sikku/DigitSum.java
@@ -0,0 +1,26 @@
+package com.sikku;
+
+public class DigitSum {
+    public static void main(String[] args) {
+        System.out.println(sumDigit("1230657480"));
+    }
+
+    public static int sumDigit(int num){
+
+        int sum =0;
+        while (num>0){
+            sum += num %10;
+            num /=10;
+        }
+        return sum;
+    }
+
+    public static int sumDigit(String num){
+
+        int sum =0;
+        for (int i=0 ;i<num.length(); i++){
+            sum += num.charAt(i) - '0';
+        }
+        return sum;
+    }
+}
