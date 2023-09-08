1912067f8ed85584e4ee4e088390fde29e803de0
Upendra <upendrasatna@gmail.com>
2023-09-08 22:11:30 +0530

    array searching added

---
+++ Array/src/com/sikku/searching/BinarySearch.java
@@ -0,0 +1,31 @@
+package com.sikku.searching;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int index = search(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 8);
+        System.out.println(index);
+    }
+
+    public static int search(int[] ar, int num) {
+        int start = 0;
+        int end = ar.length - 1;
+        int mid = (start + end) / 2;
+
+        int index = -1;
+
+        while (start <= mid && end >= mid) {
+            if (num == ar[mid]) {
+                index = mid;
+                return index;
+            } else if (num > ar[mid]) {
+                start = mid +1;
+                mid = (end + start) / 2;
+            } else {
+                end = mid -1;
+                mid = (end + start) / 2;
+            }
+        }
+        return index;
+    }
+
+}
