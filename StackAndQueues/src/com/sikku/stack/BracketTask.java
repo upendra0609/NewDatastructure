f50ee213a3eda4fd3e5fa84761fc5b7ef1aa267d
Upendra <upendrasatna@gmail.com>
2023-09-15 16:27:13 +0530

    more code on stack

--- StackAndQueues/src/com/sikku/stack/BracketTask.java
+++ StackAndQueues/src/com/sikku/stack/BracketTask.java
@@ -1,4 +1,34 @@
 package com.sikku.stack;
 
+import java.util.Stack;
+
 public class BracketTask {
+    public static void main(String[] args) {
+        Stack<Character> st = new Stack<>();
+//        String str = "{[]{(){}}()[]}";
+        String str = "{()()(){}[]}";
+        char[] c = str.toCharArray();
+
+        for (char cr : c) {
+            if (cr == '(' || cr == '{' || cr == '[') {
+                st.push(cr);
+            } else {
+                if (cr == ')' && st.peek() == '(') {
+                    st.pop();
+                } else if (cr == '}' && st.peek() == '{') {
+                    st.pop();
+                } else if (cr == ']' && st.peek() == '[') {
+                    st.pop();
+                } else {
+                    break;
+                }
+            }
+        }
+
+        if (st.isEmpty()) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
+    }
 }
