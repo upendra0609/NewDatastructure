9d8fb685e5b65baa2ba917de6fcb223a314ab07d
Upendra <upendrasatna@gmail.com>
2023-09-17 02:10:41 +0530

    more on sorting

--- TUF/Sorting/src/com/sikku/problem/largest/ifsorted/Main.java
+++ TUF/Sorting/src/com/sikku/problem/largest/ifsorted/Main.java
@@ -2,6 +2,16 @@ package com.sikku.problem.largest.ifsorted;
 
 public class Main {
     public static void main(String[] args) {
-        
+        int[] array = {1,2,3,4,5,6,7,8,8};
+        System.out.println(isSorted(array));
+    }
+
+    public static boolean isSorted(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
