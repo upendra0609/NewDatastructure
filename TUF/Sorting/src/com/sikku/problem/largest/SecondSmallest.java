678962fc28946ea9e096ddf015e20cf9b91d0095
Upendra <upendrasatna@gmail.com>
2023-09-17 01:00:17 +0530

    more on sorting

--- TUF/Sorting/src/com/sikku/problem/largest/SecondSmallest.java
+++ TUF/Sorting/src/com/sikku/problem/largest/SecondSmallest.java
@@ -1,4 +1,20 @@
 package com.sikku.problem.largest;
 
 public class SecondSmallest {
+    public static void main(String[] args) {
+        int[] array = {1, 3, 5, 7, 8, 43, 123, 57, 86};
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        for (int i : array) {
+            if (smallest > i) {
+                secondSmallest = smallest;
+                smallest = i;
+            } else if (i < secondSmallest && i > smallest) {
+                secondSmallest = i;
+            }
+        }
+        System.out.println(smallest);
+        System.out.println(secondSmallest);
+    }
 }
---
+++ TUF/Sorting/src/com/sikku/problem/largest/ifsorted/Main.java
@@ -0,0 +1,7 @@
+package com.sikku.problem.largest.ifsorted;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
