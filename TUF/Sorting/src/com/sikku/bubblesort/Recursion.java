fa9fd73762ea6a326fbd14807aa0b5dea94bef0d
Upendra <upendrasatna@gmail.com>
2023-09-16 17:15:49 +0530

    more code on sorting

--- TUF/Sorting/src/com/sikku/bubblesort/Recursion.java
+++ TUF/Sorting/src/com/sikku/bubblesort/Recursion.java
@@ -2,6 +2,34 @@ package com.sikku.bubblesort;
 
 public class Recursion {
     public static void main(String[] args) {
-        
+        int[] array = {5, 4, 3, 2};
+//        int[] array = {1, 2, 3, 4, 5};
+
+        bubble(array, array.length);
+
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
     }
+
+    public static void bubble(int[] array, int size) {
+        if (size == 1) {
+            return;
+        }
+
+        int swap = 0;
+        for (int i = 0; i < size - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                int temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                swap++;
+            }
+        }
+        if (swap == 0) {
+            return;
+        }
+        bubble(array, size - 1);
+    }
+
 }
---
+++ TUF/Sorting/src/com/sikku/insertionsort/Recursion.java
@@ -0,0 +1,7 @@
+package com.sikku.insertionsort;
+
+public class Recursion {
+    public static void main(String[] args) {
+        
+    }
+}
