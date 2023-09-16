08704221edceee93072ed08f1ced56f5579b2a04
Upendra <upendrasatna@gmail.com>
2023-09-16 14:04:11 +0530

    more code on sorting

--- TUF/Sorting/src/com/sikku/bubblesort/Main.java
+++ TUF/Sorting/src/com/sikku/bubblesort/Main.java
@@ -1,4 +1,26 @@
 package com.sikku.bubblesort;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6};
+
+        for (int i = 0; i < array.length; i++) {
+            int didSwap = 0;
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j - 1] > array[j]) {
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                    didSwap = 1;
+                }
+            }
+            if (didSwap == 0) {
+                break;
+            }
+        }
+
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
 }
---
+++ TUF/Sorting/src/com/sikku/insertionsort/Main.java
@@ -0,0 +1,29 @@
+package com.sikku.insertionsort;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {12, 22, 9, 8, 6, 4, 1};
+//        int[] array = {1,2,3,4,5,6,7,8,9};
+        insertionSort(array, array.length);
+
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+
+    }
+
+    public static void insertionSort(int[] array, int size) {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (array[j] < array[j - 1]) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
