ce676ad14c1dbc29707d1f9b90638e14e0a8c89f
Upendra <upendrasatna@gmail.com>
2023-09-19 18:25:26 +0530

    more

--- TUF/Sorting/src/com/sikku/problem/union/Main.java
+++ TUF/Sorting/src/com/sikku/problem/union/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> i = union(new int[]{1, 2, 3, 4, 5, 6}, new int[]{1,2,3,4,5,5,5,5,5, 5, 7, 8, 9, 10, 10, 10, 10});
+        ArrayList<Integer> i = union(new int[]{1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 6,11}, new int[]{1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 7, 8, 10, 10, 10,10,10,11});
 
         for (int j : i) {
             System.out.print(j + " ");
@@ -16,28 +16,37 @@ public class Main {
 
         int i = 0;
         int j = 0;
+        int itemInserted = -1;
+
         while (i < array1.length && j < array2.length) {
             if (array1[i] == array2[j]) {
                 i++;
-            } else if (array1[i] < array2[j]) {
-                unionList.add(array1[i]);
-                i++;
-            } else {
-                unionList.add(array2[j]);
+            } else if (array2[j] < array1[i]) {
+                if (array2[j] != itemInserted) {
+                    unionList.add(array2[j]);
+                    itemInserted = array2[j];
+                }
                 j++;
+            } else {
+                if (array1[i] != itemInserted) {
+                    unionList.add(array1[i]);
+                    itemInserted = array1[i];
+                }
+                i++;
             }
         }
 
         while (i < array1.length) {
-            if (array1[i - 1] != array1[i]) {
+            if (array1[i] != itemInserted) {
                 unionList.add(array1[i]);
+                itemInserted = array1[i];
             }
             i++;
         }
-
         while (j < array2.length) {
-            if (array2[j] != array2[j - 1]) {
+            if (array2[j] != itemInserted) {
                 unionList.add(array2[j]);
+                itemInserted = array2[j];
             }
             j++;
         }
