f8d7876ff017d17b3c1b04e0c035b8c2fd99855d
Upendra <upendrasatna@gmail.com>
2023-09-24 02:45:11 +0530

    more problem

--- TUF/Sorting/src/com/sikku/problem/rotateArray/Main.java
+++ TUF/Sorting/src/com/sikku/problem/rotateArray/Main.java
@@ -6,7 +6,8 @@ public class Main {
                 {5, 6, 7, 8},
                 {9, 10, 11, 12},
                 {13, 14, 15, 16}};
-        rotate(array);
+//        rotate(array);
+        rotateTUF(array);
 
         for (int[] i : array) {
             for (int j : i) {
@@ -16,6 +17,7 @@ public class Main {
         }
     }
 
+    // T.C. -> less than O(n^2)
     public static void rotate(int[][] array) {
         int n = array.length;
         for (int i = 0; i < n / 2; i++) {
@@ -25,6 +27,28 @@ public class Main {
                 array[n - j - 1][i] = array[n - i - 1][n - j - 1];
                 array[n - i - 1][n - j - 1] = array[j][n - i - 1];
                 array[j][n - i - 1] = temp;
+            }
+        }
+    }
+
+    public static void rotateTUF(int[][] array) {
+
+        //transpose the array
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array[i].length; j++) {
+                int temp = array[i][j];
+                array[i][j] = array[j][i];
+                array[j][i] = temp;
+            }
+        }
+
+        //reverse array
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length / 2; j++) {
+                int temp = array[i][j];
+                array[i][j] = array[i][array[i].length - j - 1];
+                array[i][array[i].length - j - 1] = temp;
+
             }
         }
     }
