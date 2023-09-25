c8c87072aed7bc7176bd085a86e5c816e01fcfd3
Upendra <upendrasatna@gmail.com>
2023-09-25 16:37:00 +0530

    pascal triangle

--- TUF/Sorting/src/com/sikku/problem/pascalTriangle/Main.java
+++ TUF/Sorting/src/com/sikku/problem/pascalTriangle/Main.java
@@ -1,4 +1,20 @@
 package com.sikku.problem.pascalTriangle;
 
 public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+        int r = 3;
+        System.out.println(printParticularElement(n - 1, r - 1));
+    }
+
+    public static long printParticularElement(int n, int c) {
+        long result = 1;
+
+        for (int i = 0; i < c; i++) {
+            result *= (n - i);
+            result /= (i + 1);
+        }
+
+        return result;
+    }
 }
