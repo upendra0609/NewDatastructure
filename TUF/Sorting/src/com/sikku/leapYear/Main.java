dc68029251e61e50a0671ea357f4643d468418d3
Upendra <upendrasatna@gmail.com>
2023-09-27 03:11:39 +0530

    pascal triangle

--- TUF/Sorting/src/com/sikku/leapYear/Main.java
+++ TUF/Sorting/src/com/sikku/leapYear/Main.java
@@ -2,6 +2,21 @@ package com.sikku.leapYear;
 
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println(leapYear(100));
+        System.out.println(leapYear(400));
+        System.out.println(leapYear(2004));
+    }
+
+    public static boolean leapYear(int year) {
+        boolean isLeap = true;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0 && year % 400 != 0) {
+                isLeap = false;
+            }
+        } else {
+            isLeap = false;
+        }
+        return isLeap;
     }
 }
