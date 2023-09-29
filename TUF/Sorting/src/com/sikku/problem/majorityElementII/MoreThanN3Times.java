e32ea8acea7fa10cd7367d0c353b85a8908565c7
Upendra <upendrasatna@gmail.com>
2023-09-29 18:11:53 +0530

    majority element optimal

--- TUF/Sorting/src/com/sikku/problem/majorityElementII/MoreThanN3Times.java
+++ TUF/Sorting/src/com/sikku/problem/majorityElementII/MoreThanN3Times.java
@@ -7,9 +7,11 @@ import java.util.Set;
 
 public class MoreThanN3Times {
     public static void main(String[] args) {
-        int[] array = {1, 1, 1, 3, 3, 2, 2, 2};
+        int[] array = { 2, 2};
 //        Set<Integer> set = bruteForce(array);
-        Set<Integer> set = better(array);
+//        Set<Integer> set = better(array);
+
+        Set<Integer> set = optimal(array);
 
         for (int i : set) {
             System.out.print(i + " ");
@@ -56,6 +58,53 @@ public class MoreThanN3Times {
                 result.add(array[i]);
             }
         }
+        return result;
+    }
+
+    public static Set<Integer> optimal(int[] array) {
+        Set<Integer> result = new HashSet<>();
+
+        int ele1 = 0;
+        int count1 = 0;
+        int ele2 = 0;
+        int count2 = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (count1 == 0 && array[i] != ele2) {
+                ele1 = array[i];
+                count1 = 1;
+            } else if (count2 == 0 && array[i] != ele1) {
+                ele2 = array[i];
+                count2 = 1;
+            } else if (ele1 == array[i]) {
+                count1++;
+            } else if (ele2 == array[i]) {
+                count2++;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        if (count2 > 0 || count1 > 0) {
+            count1 = 0;
+            count2 = 0;
+            //confirming if exist more than n/3 times
+            for (int i = 0; i < array.length; i++) {
+                if (ele1 == array[i]) {
+                    count1++;
+                    if (count1 > array.length / 3) {
+                        result.add(ele1);
+                    }
+                } else if (ele2 == array[i]) {
+                    count2++;
+                    if (count2 > array.length / 3) {
+                        result.add(ele2);
+                    }
+                }
+            }
+        }
+
         return result;
     }
 }
