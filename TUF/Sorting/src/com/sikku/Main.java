a9ab8fbbf8f029ad0849dd8e56016975b06ad48d
Upendra <upendrasatna@gmail.com>
2023-10-06 22:03:34 +0530

    merge interval

--- TUF/Sorting/src/com/sikku/Main.java
+++ TUF/Sorting/src/com/sikku/Main.java
@@ -2,6 +2,8 @@ package com.sikku;
 
 public class Main {
     public static void main(String[] args) {
-
+        int i = Integer.MAX_VALUE;
+        System.out.println(Integer.toBinaryString(i));
+        System.out.println(i>>1);
     }
 }
--- TUF/Sorting/src/com/sikku/problem/mergeOverlappingIntervals/Main.java
+++ TUF/Sorting/src/com/sikku/problem/mergeOverlappingIntervals/Main.java
@@ -1,32 +1,38 @@
 package com.sikku.problem.mergeOverlappingIntervals;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] array = {{3, 4}, {6, 7}, {1, 7}, {1, 3},{1,3},{1,2},{1,9}};
+        int[][] array = {{5, 2}, {16, 21}, {1, 3}, {2, 6}, {8, 10}, {9, 19}, {15, 18}};
 
-        Arrays.sort(array, (i1, i2) -> {
-            int x = i1[0];
-            int y = i1[1];
-            int p = i2[0];
-            int q = i2[1];
-            if (x < p) {
-                return -1;
-            } else if (x == p && y < q) {
-                return -1;
-            } else if (p == x && q==y) {
-                return 0;
-            } else {
-                return 1;
-            }
-        });
-
-        for (int[] i : array) {
+        int[][] ar = overlappingInterval(array);
+        for (int[] i : ar) {
             for (int j : i) {
-                System.out.print(j);
+                System.out.print(j + " ");
             }
             System.out.println();
         }
+    }
+
+    public static int[][] overlappingInterval(int[][] array) {
+        Stack<int[]> list = new Stack<>();
+        Arrays.sort(array, (I1, I2) -> I1[0] - I2[0]);
+        list.push(new int[]{array[0][0], array[0][1]});
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i][0] <= list.peek()[1]) {
+//                if (array[i][0] <= list.peek()[0]) {
+//                    list.peek()[0] = array[i][0];
+//                }
+                if (array[i][1] > list.peek()[1]) {
+                    list.peek()[1] = array[i][1];
+                }
+            } else {
+                list.push(new int[]{array[i][0], array[i][1]});
+            }
+        }
+        return list.toArray(new int[list.size()][]);
     }
 }
