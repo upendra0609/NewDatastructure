78d2da6566f5da97a59b653ed2a0632b5ff11445
Upendra <upendrasatna@gmail.com>
2023-10-09 08:40:57 +0530

    reverse pair

--- TUF/Sorting/src/com/sikku/Main.java
+++ TUF/Sorting/src/com/sikku/Main.java
@@ -2,10 +2,12 @@ package com.sikku;
 
 public class Main {
     public static void main(String[] args) {
-        int i = Integer.MAX_VALUE;
+        Integer i = Integer.MAX_VALUE;
+        Integer j = 12;
         System.out.println(Integer.toBinaryString(i));
         System.out.println(i>>1);
-
+        System.out.println(System.identityHashCode(i));
+        System.out.println(j.hashCode());
 
     }
 }
