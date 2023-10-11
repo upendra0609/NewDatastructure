7221a48df2d754e1d32949410fa843c6b1c6b3c5
Upendra <upendrasatna@gmail.com>
2023-10-11 20:10:58 +0530

    binary search

---
+++ TUF/Sorting/src/com/sikku/binarySearching/kokoEatingBanana/Main.java
@@ -0,0 +1,15 @@
+package com.sikku.binarySearching.kokoEatingBanana;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1};
+        int x =0;
+        x += Math.ceil((double) 11 /2);
+        System.out.println(x);
+    }
+}
+
+
--- TUF/Sorting/src/com/sikku/binarySearching/squareRoot/Main.java
+++ TUF/Sorting/src/com/sikku/binarySearching/squareRoot/Main.java
@@ -2,13 +2,27 @@ package com.sikku.binarySearching.squareRoot;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(root(26));
+        System.out.println(root(25));
+        System.out.println(root(46));
+        System.out.println(root(66));
+        System.out.println(root(0));
+        System.out.println(root(1));
     }
 
-    public static int root(long n) {
+    //optimal
+    public static int root(int n) {
         int low = 1;
-        long high = n;
-        int mid = low + (int) (high - low) / 2;
-        
+        int high = n;
+
+        while (low <= high) {
+            int mid = (high + low) / 2;
+            if (mid * mid <= n) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return high;
     }
 }
