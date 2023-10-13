a0dbabf4db75b537235731a3597e208ec77a077f
Upendra <upendrasatna@gmail.com>
2023-10-13 10:41:28 +0530

    aggressive cow

--- TUF/Sorting/src/com/sikku/binarySearching/aggressiveCow/Main.java
+++ TUF/Sorting/src/com/sikku/binarySearching/aggressiveCow/Main.java
@@ -30,5 +30,31 @@ public class Main {
         return start;
     }
 
-    
+    public static int optimal(int[] array, int cows) {
+        Arrays.sort(array);
+        int min = array[0];
+        int max = array[array.length - 1];
+        int start = 0;
+        int high = max - min;
+        int ans = 0;
+
+        while (start <= high) {
+            int count = 1;
+            int prev = 0;
+            int mid = (start + high) / 2;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] - array[prev] >= mid) {
+                    prev = i;
+                    count++;
+                }
+            }
+            if (count < cows) {
+                high = mid - 1;
+            } else {
+                start = mid + 1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
 }
