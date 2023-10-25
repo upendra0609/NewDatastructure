6270caa75d4a0d724ca2dfbdd5b69471fc9f43ae
Upendra <upendrasatna@gmail.com>
2023-10-25 16:14:20 +0530

    a

--- TUF/Sorting/src/com/sikku/Main.java
+++ TUF/Sorting/src/com/sikku/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-     /*   Integer i = Integer.MAX_VALUE;
+        Integer i = Integer.MAX_VALUE;
         Integer j = 12;
         System.out.println(Integer.toBinaryString(i));
         System.out.println(i>>1);
@@ -14,13 +14,13 @@ public class Main {
         int[] ar = {1,2};
         int x = Arrays.binarySearch(ar,2);
 
-      */
 
-        int i =4;
-        int j =5;
-        i = i^j;
-        j = i^j;
-        i = i^j;
-        System.out.println("i "+i+"\nj "+j);
+
+//        int i =4;
+//        int j =5;
+//        i = i^j;
+//        j = i^j;
+//        i = i^j;
+//        System.out.println("i "+i+"\nj "+j);
     }
 }
