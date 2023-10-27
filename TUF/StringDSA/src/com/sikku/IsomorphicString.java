b49995a2661c33a2d301f87f3089a285023f402f
Upendra <upendrasatna@gmail.com>
2023-10-27 17:51:59 +0530

    string

--- TUF/StringDSA/src/com/sikku/IsomorphicString.java
+++ TUF/StringDSA/src/com/sikku/IsomorphicString.java
@@ -1,4 +1,33 @@
 package com.sikku;
 
+import java.util.HashMap;
+
 public class IsomorphicString {
+    public static void main(String[] args) {
+        System.out.println(isIsomorphic("egg", "foo"));
+        System.out.println(isIsomorphic("bar", "foo"));
+        System.out.println(isIsomorphic("paper", "title"));
+    }
+
+    public static boolean isIsomorphic(String s1, String s2) {
+        HashMap<Character, Character> map1 = new HashMap<>();
+
+        for (int i = 0; i < s1.length(); i++) {
+            char ch = s1.charAt(i);
+            char ch2 = s2.charAt(i);
+
+            if (!map1.containsKey(ch)) {
+                if (!map1.containsValue(ch2)) {
+                    map1.put(ch, ch2);
+                } else {
+                    return false;
+                }
+            } else {
+                if (map1.get(ch) != ch2) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
