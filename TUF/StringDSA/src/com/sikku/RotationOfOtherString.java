96a9d26da15fa77dfc785adfef62656b94700899
Upendra <upendrasatna@gmail.com>
2023-10-28 16:04:43 +0530

    rotation of another string

--- TUF/StringDSA/src/com/sikku/RotationOfOtherString.java
+++ TUF/StringDSA/src/com/sikku/RotationOfOtherString.java
@@ -2,6 +2,24 @@ package com.sikku;
 
 public class RotationOfOtherString {
     public static void main(String[] args) {
+        System.out.println(brute("abcde","cdeab"));
+        System.out.println(brute("abcde","abced"));
+    }
+
+    public static boolean brute(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            StringBuilder newStr = new StringBuilder(s.substring(i,n)+ s.substring(0,i));
+            if(goal.contentEquals(newStr)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean optimal(String s, String goal){
 
     }
 }
