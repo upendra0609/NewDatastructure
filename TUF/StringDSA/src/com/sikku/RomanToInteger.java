8eb1509601ed3ed90965bf744942e45577f20e14
Upendra <upendrasatna@gmail.com>
2023-10-29 21:14:16 +0530

    Char By Frequency

--- TUF/StringDSA/src/com/sikku/RomanToInteger.java
+++ TUF/StringDSA/src/com/sikku/RomanToInteger.java
@@ -2,7 +2,20 @@ package com.sikku;
 
 public class RomanToInteger {
     public static void main(String[] args) {
-
+        System.out.println(getInt("I"));
+        System.out.println(getInt("IV"));
     }
 
+    public static int getInt(String s) {
+        short i = 1;
+
+        switch (i){
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+            default:
+                return 3;
+        }
+    }
 }
