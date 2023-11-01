96d4fe57f105e1f2b20d3a2c1fc3f6da8758e4e1
Upendra <upendrasatna@gmail.com>
2023-11-01 16:11:35 +0530

    linked list

--- TUF/LinkedList/src/com/sikku/LinkedListUse.java
+++ TUF/LinkedList/src/com/sikku/LinkedListUse.java
@@ -1,5 +1,7 @@
 package com.sikku;
 
+import java.util.Scanner;
+
 public class LinkedListUse {
 
     public static Node<Integer> createLinkedList() {
@@ -10,8 +12,7 @@ public class LinkedListUse {
         n2.next = n3;
         Node<Integer> n4 = new Node<>(40);
         n3.next = n4;
-        Node<Integer> n5 = new Node<>(50);
-        n4.next = n5;
+        n4.next = new Node<>(50);
         return n1;
     }
 
@@ -22,6 +23,21 @@ public class LinkedListUse {
         }
     }
 
+    public static int print(Node<Integer> head, int position) {
+        Node<Integer> temp = head;
+        int i = 0;
+        int result = 0;
+        while (temp != null) {
+            if (i == position) {
+                result = temp.data;
+                break;
+            }
+            i++;
+            temp = temp.next;
+        }
+        return result;
+    }
+
     public static void increment(Node<Integer> head) {
         while (head != null) {
             head.data++;
@@ -29,9 +45,79 @@ public class LinkedListUse {
         }
     }
 
+    public static int size(Node<Integer> head) {
+        int n = 0;
+        Node<Integer> temp = head;
+        while (temp.next != null) {
+            n++;
+            temp = temp.next;
+        }
+        return n;
+    }
+
+    public static Node<Integer> insert(Node<Integer> head, int value, int position) {
+        if (position == 0) {
+            Node<Integer> n = new Node<>(value);
+            n.next = head;
+            return n;
+        }
+
+        Node<Integer> temp = head;
+        int tempPosition = 1;
+        while (tempPosition <= position) {
+            if (position == tempPosition) {
+                Node<Integer> n = new Node<>(value);
+                n.next = temp.next;
+                temp.next = n;
+                return head;
+            }
+            tempPosition++;
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    public static void deleteNode(Node<Integer> node) {
+    }
+
+    public static Node<Integer> takeInput() {
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number");
+        int num = sc.nextInt();
+
+        Node<Integer> head = null;
+        Node<Integer> tail = null;
+
+        while (num != -1) {
+            Node<Integer> node = new Node<>(num);
+            if (head == null) {
+                head = node;
+            } else {
+                tail.next = node;
+            }
+            tail = node;
+            System.out.println("Enter number");
+            num = sc.nextInt();
+            count++;
+        }
+        System.out.println(count);
+        return head;
+    }
+
     public static void main(String[] args) {
         Node<Integer> head = createLinkedList();
-        increment(head);
+//        increment(head);
+//        print(head);
+//        System.out.println(size(head));
+
+//        System.out.println(print(head, 2));
+//        Node<Integer> head = takeInput();
+        head = insert(head, 1, 0);
+        head = insert(head,1,6);
         print(head);
+
+
     }
 }
--- TUF/LinkedList/src/com/sikku/Node.java
+++ TUF/LinkedList/src/com/sikku/Node.java
@@ -4,6 +4,10 @@ public class Node<T> {
     T data;
     Node<T> next;
 
+    public Node() {
+
+    }
+
     public Node(T data) {
         this.data = data;
     }
