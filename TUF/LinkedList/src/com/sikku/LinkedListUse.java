ff817b9deab046879f23e7d30fa85129027d72b8
Upendra <upendrasatna@gmail.com>
2023-11-03 22:26:15 +0530

    reverse linked list

--- TUF/LinkedList/src/com/sikku/LinkedListUse.java
+++ TUF/LinkedList/src/com/sikku/LinkedListUse.java
@@ -188,7 +188,19 @@ public class LinkedListUse {
     }
 
     public static boolean linkedListCycleOptimal(Node<Integer> head) {
-        
+        Node<Integer> slow = head;
+        Node<Integer> fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+
     }
 
     public static void main(String[] args) {
