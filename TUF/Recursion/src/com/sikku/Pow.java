c4019169371face8e68c7f7d9ccb3c87d6ebd029
Upendra <upendrasatna@gmail.com>
2023-11-10 21:29:32 +0530

    recursion

--- TUF/Recursion/src/com/sikku/Pow.java
+++ TUF/Recursion/src/com/sikku/Pow.java
@@ -1,4 +1,13 @@
 package com.sikku;
 
 public class Pow {
+    public static void main(String[] args) {
+        System.out.println(pow(2,2));
+    }
+
+    public static double pow(double x, int n) {
+        if (n == 1) return x;
+
+        return x * pow(x, n - 1);
+    }
 }
