2cfb8c6a708b2fce6684e02059cef8996271cc83
Upendra <upendrasatna@gmail.com>
2023-11-11 21:26:30 +0530

    recursion

--- TUF/Recursion/src/com/sikku/BinaryString.java
+++ TUF/Recursion/src/com/sikku/BinaryString.java
@@ -8,45 +8,45 @@ public class BinaryString {
 
     }
 
-    private static List<String> list= new ArrayList<>();
-    public static List< String > generateString(int n) {
-        if(n==1){
-            List<String> tempList = new ArrayList<>();
+
+    public static List<String> generateString(int n) {
+        List<String> tempList = new ArrayList<>();
+        if (n == 1) {
             tempList.add("0");
             tempList.add("1");
             return tempList;
         }
 
-        List<String> l = generateString(n-1);
-        for(String s: l){
-            if(s.endsWith("0")){
-                list.add(s+"1");
-                list.add(s+"0");
-            }else{
+        tempList = generateString(n - 1);
+        List<String> list = new ArrayList<>();
+        for (String s : tempList) {
+            if (s.endsWith("0")) {
+                list.add(s + "1");
+                list.add(s + "0");
+            } else {
                 list.add("0");
             }
         }
         return list;
-
     }
 }
 
-
+//another approach
 class Solution {
 
-    public static List< String > generateString(int N) {
+    public static List<String> generateString(int N) {
 
         List<String> ans = new ArrayList<String>();
 
-        helper(N,"", ans);
+        helper(N, "", ans);
 
         return ans;
 
     }
 
-    public static void helper(int N, String curr, List<String> ans){
+    public static void helper(int N, String curr, List<String> ans) {
 
-        if(N==0){
+        if (N == 0) {
 
             ans.add(curr);
 
@@ -54,15 +54,15 @@ class Solution {
 
         }
 
-        if(curr.isEmpty() || curr.charAt(curr.length()-1)=='0'){
+        if (curr.isEmpty() || curr.charAt(curr.length() - 1) == '0') {
 
-            helper(N-1,curr+"0", ans);
+            helper(N - 1, curr + "0", ans);
 
-            helper(N-1,curr+"1", ans);
+            helper(N - 1, curr + "1", ans);
 
-        }else{
+        } else {
 
-            helper(N-1,curr+"0", ans);
+            helper(N - 1, curr + "0", ans);
 
         }
 
