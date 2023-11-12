dcd6b9752ff77c67f8ce97aed5c65ba8fd7f4753
Upendra <upendrasatna@gmail.com>
2023-11-12 14:13:54 +0530

    recursion

--- TUF/Recursion/src/com/sikku/GenerateParentheses.java
+++ TUF/Recursion/src/com/sikku/GenerateParentheses.java
@@ -4,11 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateParentheses {
-    public static void main(String[] args) {
+    static ArrayList<String> result = new ArrayList<>();
 
+    public static void main(String[] args) {
+        generate(4, 0, 0, "");
+
+        for (String s : result) {
+            System.out.print(s + "  ");
+        }
     }
 
-    public static List<String> generate(int n) {
-        return null;
+    public static void generate(int n, int open, int close, String str) {
+        if (open == n && close == n) {
+            result.add(str);
+//            str = "";
+        }
+
+        if (open < n) {
+//            str = str + "(";
+            generate(n, open + 1, close, str+"(");
+        }
+
+        if (close < open) {
+//            str = str + ")";
+            generate(n, open, close + 1, str+")");
+        }
     }
 }
