8720c69e562a5c2f7afe0a8634ee1e543aabfa1f
Upendra <upendrasatna@gmail.com>
2023-12-07 17:44:36 +0530

    minWindow

--- TUF/SlidingWindow/src/com/sikku/minimumWindowSubstring/Main.java
+++ TUF/SlidingWindow/src/com/sikku/minimumWindowSubstring/Main.java
@@ -1,11 +1,14 @@
 package com.sikku.minimumWindowSubstring;
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(minWindow("ADOBECODEBANC", "ABC"));
-        System.out.println("2 "+ minWindow("A", "AA"));
+        System.out.println("a: "+minWindowOptimal("ADOBECODEBANC", "ABC"));
+//        System.out.println(minWindowOptimal("A", "AA"));
 //        StringBuilder s = new StringBuilder("baa");
 //        System.out.println(s.indexOf("a"));
+
     }
 
     public static String minWindow(String s, String t) {
@@ -47,5 +50,60 @@ public class Main {
             }
         }
         return true;
+    }
+
+    public static String minWindowOptimal(String s, String t) {
+        if (s.contains(t)) {
+            return t;
+        }
+        int m = s.length();
+        int n = t.length();
+        HashMap<Character, Integer> hash = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            char ch = t.charAt(i);
+            hash.put(ch, hash.getOrDefault(ch, 0) + 1);
+        }
+
+        int count = hash.size();
+        int right = 0;
+        int left = 0;
+        // int ans = Integer.MAX_VALUE;
+        String ans = s;
+        while (right < m) {
+            char ch = s.charAt(right);
+            if (hash.containsKey(ch)) {
+                int tempCount = hash.get(ch);
+                if (tempCount == 1) {
+                    count--;
+                }
+                hash.put(ch, hash.getOrDefault(ch, 0) - 1);
+            }
+            right++;
+
+            while (count == 0) {
+//                System.out.println("right: " + right + "\nleft: " + left);
+                // System.out.println(s.substring(left, right));
+                String tempAns = s.substring(left, right);
+                System.out.println(tempAns);
+                if(ans.length()>=tempAns.length()){
+                    ans = tempAns;
+                }
+                // ans = Math.min(ans, right - left);
+                char chRemove = s.charAt(left);
+                if (hash.containsKey(chRemove)) {
+                    int tempCount = hash.get(chRemove);
+                    if (tempCount == 0) {
+                        count++;
+                    }
+                    hash.put(chRemove, hash.getOrDefault(chRemove, 0) + 1);
+                }
+                left++;
+            }
+        }
+//        System.out.println(ans);
+        if(ans.equals(s)){
+            ans = "";
+        }
+        return ans;
     }
 }
