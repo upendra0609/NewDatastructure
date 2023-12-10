5c006b83aeffbb111bb52b91816165a89764b413
Upendra <upendrasatna@gmail.com>
2023-12-10 16:20:41 +0530

    preorder traversal

--- TUF/BinaryTree/src/com/sikku/traversal/preorder/Main.java
+++ TUF/BinaryTree/src/com/sikku/traversal/preorder/Main.java
@@ -1,6 +1,23 @@
 package com.sikku.traversal.preorder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    private List<Integer> list = new ArrayList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        traverse(root);
+        return list;
+    }
+
+    public void traverse(TreeNode root) {
+        if (root == null) return;
+
+        list.add(root.val);
+        traverse(root.left);
+        traverse(root.right);
+    }
 }
 
 class TreeNode {
