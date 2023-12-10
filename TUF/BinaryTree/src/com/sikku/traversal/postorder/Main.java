37d66b291b241701c4efba9e9b394ef960a5d019
Upendra <upendrasatna@gmail.com>
2023-12-10 16:19:02 +0530

    traversal

---
+++ TUF/BinaryTree/src/com/sikku/traversal/postorder/Main.java
@@ -0,0 +1,25 @@
+package com.sikku.traversal.postorder;
+
+public class Main {
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
---
+++ TUF/BinaryTree/src/com/sikku/traversal/preorder/Main.java
@@ -0,0 +1,24 @@
+package com.sikku.traversal.preorder;
+
+public class Main {
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
