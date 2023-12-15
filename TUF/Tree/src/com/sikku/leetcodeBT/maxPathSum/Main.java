c35154f7bcbd23d05c23a7772222f5e31260e570
Upendra <upendrasatna@gmail.com>
2023-12-15 16:48:31 +0530

    Balanced BT

--- TUF/Tree/src/com/sikku/leetcodeBT/maxPathSum/Main.java
+++ TUF/Tree/src/com/sikku/leetcodeBT/maxPathSum/Main.java
@@ -1,4 +1,41 @@
 package com.sikku.leetcodeBT.maxPathSum;
 
 public class Main {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private int max = -1001;
+
+    public int maxPathSum(TreeNode root) {
+        int temp = maxm(root);
+        return Math.max(temp, max);
+    }
+
+    public int maxm(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int lAns = Math.max(0, maxm(root.left));
+        int rAns = Math.max(0, maxm(root.right));
+        max = Math.max(max, lAns + rAns + root.val);
+        return (root.val + Math.max(lAns, rAns));
+    }
 }
