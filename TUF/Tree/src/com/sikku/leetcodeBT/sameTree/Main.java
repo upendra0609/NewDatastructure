ccf210e5bfafbec42f3e860b7395ce55126ae4ce
Upendra <upendrasatna@gmail.com>
2023-12-16 14:06:23 +0530

    identical tree

---
+++ TUF/Tree/src/com/sikku/leetcodeBT/sameTree/Main.java
@@ -0,0 +1,43 @@
+package com.sikku.leetcodeBT.sameTree;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isSameTreeM1(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+
+        if (p.val == q.val) {
+            return isSameTreeM1(p.left, q.left) && isSameTreeM1(p.right, q.right);
+        } else {
+            return false;
+        }
+    }
+
+
+
+
+
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
