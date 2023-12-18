97bc96101fd1224aa0b367f81fda48822d7368cd
Upendra <upendrasatna@gmail.com>
2023-12-18 13:02:05 +0530

    boundary traversal

--- TUF/Tree/src/com/sikku/Demo.java
+++ TUF/Tree/src/com/sikku/Demo.java
@@ -1,15 +1,17 @@
 package com.sikku;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 public class Demo {
     public static void main(String[] args) {
-        Integer[] ar = {1, 2, 3, 4, 5};
-        Arrays.sort(ar, (I, J) -> J.compareTo(I));
-
-        String[] arr = {"1", "9", "20", "2"};
-        System.out.println(largestNum(arr));
-
+//        Integer[] ar = {1, 2, 3, 4, 5};
+//        Arrays.sort(ar, (I, J) -> J.compareTo(I));
+//
+//        String[] arr = {"1", "9", "20", "2"};
+//        System.out.println(largestNum(arr));
     }
 
     public static String largestNum(String[] arr) {
--- TUF/Tree/src/com/sikku/leetcodeBT/boundaryTraversal/Main.java
+++ TUF/Tree/src/com/sikku/leetcodeBT/boundaryTraversal/Main.java
@@ -23,6 +23,15 @@ public class Main {
         root.left = n1;
         root.right = n6;
         n1.left = n2;
+        n2.right = n3;
+        n3.left = n4;
+        n3.right = n5;
+        n6.right = n7;
+        n7.left = n8;
+        n8.right = n10;
+        n8.left = n9;
+
+        traverse(root);
 
     }
 
@@ -32,21 +41,48 @@ public class Main {
             return list;
         }
         list.add(root.val);
-        TreeNode leftNode = root;
 
-        while (leftNode != null) {
-            if (root.left == null && root.right == null) {
-                break;
-            } else {
-                list.add(leftNode.val);
-            }
+        TreeNode leftNode = root.left;
+        //Left traversal
+        while (leftNode.left != null || leftNode.right != null) {
+            list.add(leftNode.val);
             if (leftNode.left != null) {
                 leftNode = leftNode.left;
-            } else if (leftNode.right != null) {
+            } else {
                 leftNode = leftNode.right;
             }
         }
 
+        //leaf node
+        leafNode(root, list);
+
+        //right traversal
+        TreeNode rightNode = root.right;
+        Stack<Integer> stack = new Stack<>();
+
+        while (rightNode.right != null || rightNode.left != null) {
+            stack.add(rightNode.val);
+            if (rightNode.right != null) {
+                rightNode = rightNode.right;
+            } else {
+                rightNode = rightNode.left;
+            }
+        }
+        while (!stack.isEmpty()) {
+            list.add(stack.pop());
+        }
+        System.out.println(list);
         return list;
+    }
+
+    private static void leafNode(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        leafNode(root.left, list);
+        if (root.left == null && root.right == null) {
+            list.add(root.val);
+        }
+        leafNode(root.right, list);
     }
 }
